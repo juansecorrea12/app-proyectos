605ca13aceddccab5a9479096ffbb107bf0fc064
juan correa <juansecorrea12@gmail.com>
2021-06-23 14:21:04 -0500

    add file and route for index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).send({message: 'Working!!'});
+    res.render('index');
 })
 
 module.exports = router;
